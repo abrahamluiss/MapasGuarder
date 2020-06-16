917557e8b8dac54c92784ac7432f4cf80e5a7bab
abrahamluiss <abraham105@hotmail.com>
2020-06-16 18:37:40 -0500

    Diferencia entre usuer y guarderia

--- app/src/main/java/com/guarderiashyo/guarderiashyo/RegisterActivity.java
+++ app/src/main/java/com/guarderiashyo/guarderiashyo/RegisterActivity.java
@@ -2,13 +2,20 @@ package com.guarderiashyo.guarderiashyo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    SharedPreferences mPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mPref = getApplicationContext().getSharedPreferences("typeUser", MODE_PRIVATE);
+        String seleccionarUser = mPref.getString("user", "");
+        Toast.makeText(this, "El valor es: "+seleccionarUser, Toast.LENGTH_SHORT).show();
+
     }
 }
