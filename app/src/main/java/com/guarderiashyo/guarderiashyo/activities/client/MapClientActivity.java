f0a54284c95220a3aea0a7e2627dbd9e5ffa971a
abrahamluiss <abraham105@hotmail.com>
2020-06-16 21:34:15 -0500

    Agregando el api y visualizacion del googleMaps de client

--- app/src/main/java/com/guarderiashyo/guarderiashyo/activities/client/MapClientActivity.java
+++ app/src/main/java/com/guarderiashyo/guarderiashyo/activities/client/MapClientActivity.java
@@ -7,28 +7,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.guarderiashyo.guarderiashyo.R;
 import com.guarderiashyo.guarderiashyo.activities.InicioActivity;
 import com.guarderiashyo.guarderiashyo.providers.AuthProvider;
 
-public class MapClientActivity extends AppCompatActivity {
+public class MapClientActivity extends AppCompatActivity implements OnMapReadyCallback {
     Button btnCerrarSesion;
     AuthProvider mAuthProvider;
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
-        btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
-        mAuthProvider = new AuthProvider();
-        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuthProvider.logout();
-                Intent i = new Intent(MapClientActivity.this, InicioActivity.class);
-                startActivity(i);
-                finish();
-            }
-        });
+
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    void logout(){
+        mAuthProvider.logout();
+        Intent i = new Intent(MapClientActivity.this, InicioActivity.class);
+        startActivity(i);
+        finish();
     }
 }
