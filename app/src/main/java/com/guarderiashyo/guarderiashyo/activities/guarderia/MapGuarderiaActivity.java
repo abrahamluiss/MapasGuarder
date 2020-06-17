8e41b171579bbbfe5f3ee18e4a3c4d473d71e7e7
abrahamluiss <abraham105@hotmail.com>
2020-06-16 21:29:01 -0500

    Agregando el api y visualizacion del googleMaps

--- app/src/main/java/com/guarderiashyo/guarderiashyo/activities/guarderia/MapGuarderiaActivity.java
+++ app/src/main/java/com/guarderiashyo/guarderiashyo/activities/guarderia/MapGuarderiaActivity.java
@@ -7,18 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.guarderiashyo.guarderiashyo.R;
 import com.guarderiashyo.guarderiashyo.activities.InicioActivity;
 import com.guarderiashyo.guarderiashyo.activities.client.MapClientActivity;
 import com.guarderiashyo.guarderiashyo.providers.AuthProvider;
 
-public class MapGuarderiaActivity extends AppCompatActivity {
+public class MapGuarderiaActivity extends AppCompatActivity implements OnMapReadyCallback {
     Button btnCerrarSesion;
     AuthProvider mAuthProvider;
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_guarderia);
+
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+
         btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
         mAuthProvider = new AuthProvider();
         btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +42,14 @@ public class MapGuarderiaActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
