ddee806494e280f5f732e0b32d9b0f2e5e93b2e1
abrahamluiss <abraham105@hotmail.com>
2020-06-16 22:56:56 -0500

    Ubicacion en tiempo real de guarderia

--- app/src/main/java/com/guarderiashyo/guarderiashyo/activities/guarderia/MapGuarderiaActivity.java
+++ app/src/main/java/com/guarderiashyo/guarderiashyo/activities/guarderia/MapGuarderiaActivity.java
@@ -1,18 +1,37 @@
 package com.guarderiashyo.guarderiashyo.activities.guarderia;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.pm.ActivityInfoCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.guarderiashyo.guarderiashyo.R;
 import com.guarderiashyo.guarderiashyo.activities.InicioActivity;
 import com.guarderiashyo.guarderiashyo.activities.client.MapClientActivity;
@@ -25,6 +44,28 @@ public class MapGuarderiaActivity extends AppCompatActivity implements OnMapRead
 
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
+
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocation;
+
+    private final static int LOCATION_REQUEST_CODE = 1;//actua como bandera para solicitar los permisos de ubicacion
+
+    LocationCallback mLocationCallback = new LocationCallback(){//si se mueve lo registra
+        @Override
+        public void onLocationResult(LocationResult locationResult){
+            for(Location location: locationResult.getLocations()){
+                if(getApplicationContext() != null){
+                    //obtener la localizacion del cliente en tiempo real
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder()
+                                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                                    .zoom(16f)
+                                    .build()
+                    ));
+                }
+            }
+        }
+    };
 
 
     @Override
@@ -39,6 +80,8 @@ public class MapGuarderiaActivity extends AppCompatActivity implements OnMapRead
 
         mAuthProvider = new AuthProvider();
 
+
+        mFusedLocation = LocationServices.getFusedLocationProviderClient(this);//iniciar o detener la ubicacion de user
 
 
     }
@@ -62,6 +105,63 @@ public class MapGuarderiaActivity extends AppCompatActivity implements OnMapRead
 
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(5);
+
+        startLocaction();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == LOCATION_REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                }
+            }
+        }
+    }
+
+    private void startLocaction(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+
+            }
+            else{
+                checkLocationPermissions();
+            }
+        } else {
+            mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+
+        }
+    }
+
+    private  void checkLocationPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Proporciona los permisos para continuar")
+                        .setMessage("Esta aplicacion requiere de los permisos de ubicacion para poder utilizarse")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MapGuarderiaActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+            else{
+                ActivityCompat.requestPermissions(MapGuarderiaActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            }
+        }
     }
 
     void logout(){
@@ -69,5 +169,6 @@ public class MapGuarderiaActivity extends AppCompatActivity implements OnMapRead
         Intent i = new Intent(MapGuarderiaActivity.this, InicioActivity.class);
         startActivity(i);
         finish();
+
     }
 }
